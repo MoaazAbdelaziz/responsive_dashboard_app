2243378103148ea6b250cfb3f92f8b8ccc1a2f9d
MoaazAbdelaziz <moaazwork1@gmail.com>
2024-12-01 16:24:46 +0200

    -Added isactive to check if the selected index is active or not to change it is style

--- lib/views/widgets/drawer_item.dart
+++ lib/views/widgets/drawer_item.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:responsive_dashboard/core/models/drawer_item_model.dart';
-import 'package:responsive_dashboard/core/utils/app_styles.dart';
+import 'package:responsive_dashboard/views/widgets/active_inactive_drawer_item.dart';
 
 class DrawerItem extends StatelessWidget {
-  const DrawerItem({super.key, required this.drawerItemModel});
+  const DrawerItem({
+    super.key,
+    required this.drawerItemModel,
+    required this.isActive,
+  });
   final DrawerItemModel drawerItemModel;
+  final bool isActive;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(drawerItemModel.image),
-      title: Text(
-        drawerItemModel.title,
-        style: AppStyles.styleRegular16,
-      ),
-    );
+    return isActive
+        ? ActiveDrawerItem(drawerItemModel: drawerItemModel)
+        : InActiveDrawerItem(drawerItemModel: drawerItemModel);
   }
 }
